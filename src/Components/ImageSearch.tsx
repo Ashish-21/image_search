fb0e445084a5c8930662bd234dd8dadb2808de21
ashishChandwani <ashish.chandwani@adapty.com>
2022-07-25 11:43:58 +0530

    Calling and Debouncing the API

--- src/Components/ImageSearch.tsx
+++ src/Components/ImageSearch.tsx
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import SearchIcon from "@mui/icons-material/Search";
 import { InputAdornment } from "@mui/material";
 import { Container } from "@mui/system";
+import { createApi } from "unsplash-js";
+import { debounce } from "lodash";
+
+const api = createApi({
+  accessKey: "a48Tv5EtAU1NQrAYUionrhJ7CxpL3rJMp_hH00LjFF8",
+});
 
 function ImageSearch() {
   const [searchKey, setSearchKey] = useState("");
+  const [images, setImages] = useState<any>();
 
   //search input handler
   const searchKeyHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchKey(event?.currentTarget.value);
   };
+
+  //Calling the unsplash api  and fetching Images
+  const fetchImages = async (key: string, cb: any) => {
+    const res = await api.search.getPhotos({
+      query: key,
+      orientation: "landscape",
+    });
+    cb(res);
+  };
+
+  //debouncing the api calls
+  const debouncedFetchImages = useCallback(
+    debounce((query: string, cb: any) => {
+      fetchImages(query, cb);
+    }, 500),
+    []
+  );
+
+  useEffect(() => {
+    debouncedFetchImages(searchKey, (res: any) => {
+      console.log(res);
+      if (res && res.response && res.response.results) {
+        setImages(res.response.results);
+      }
+    });
+  }, [searchKey]);
 
   return (
     <div>
--- src/Pages/ImageSearchPage.tsx
+++ src/Pages/ImageSearchPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { Typography } from "@mui/material";
+import ImageSearch from "../Components/ImageSearch";
 
 function ImageSearchPage() {
   return (
@@ -11,6 +12,7 @@ function ImageSearchPage() {
           <Typography variant="h3" py={4}>
             GO Image Search
           </Typography>
+          <ImageSearch />
         </Box>
       </Container>
     </>
